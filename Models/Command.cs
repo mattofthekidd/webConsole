f6c16528a712c158ee456c9b83d50b524544f585
Matthew Kidd <mattofthekidd@users.noreply.github.com>
2022-11-19 14:53:50 -0700

    create main service

---
+++ Models/Command.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace webConsole.Models {
+    public class Command {
+        public string name { get; }
+        public string description { get; }
+        public IEnumerable<string> options { get; }
+
+        public string function { get; }
+    }
+}
---
+++ Services/MainService.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace webConsole.Services {
+    
+    public interface IMainService {
+
+        public string runCommand(string command, IEnumerable<string> options);
+        public List<string> help(string command);
+    }
+    public class MainService : IMainService {
+        public string runCommand(string command, IEnumerable<string> options) {
+            
+            return "";
+        }
+
+        public List<string> help(string command) {
+
+            var resyu
+            return ;
+        }
+    }
+}
